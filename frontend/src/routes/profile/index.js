d45da2063e7458191b41fb8f1e259b93ff6c09ab
Nursultan Akhmetzhanov <nursultan1998@gmail.com>
2019-11-01 14:35:18 +0600

    fix(frontend): deleted Nartay's changes

--- frontend/src/routes/profile/index.js
+++ frontend/src/routes/profile/index.js
@@ -2,53 +2,27 @@ import React from 'react'
 import './index.css'
 
 const Profile = ()=>{
-    return(
-        <div>
-            <div className = "title">
-                <h3>Profile
-                    <br/>
-                    <h6>Zhanybekov Nartay</h6>
-                </h3>
-                <p className = 'email'>nartay.zhanybekov@nu.edu.kz</p>
+    return (
+        <div className="profile">
+            <h1 className="profile-title">Profile</h1>
+            <div className="profile-row">
+                <div className="profile-key">First Name:</div>
+                <div className="profile-value">Nursultan</div>
             </div>
-            <ul className = 'menu'>
-                <li>
-                    <p>Orders</p>
-                </li>
-                <li>
-                    <p>Service desk </p>
-                </li>
-                <li>
-                    <p>Finance</p>
-                </li>
-                <li>
-                    <p>Settings</p>
-                </li>
-                <li>
-                    <p>My cards</p>
-                </li>
-
-                <li>
-                    <p>Passengers</p>
-                </li>
-            </ul>
-            <div className = 'ticket'>
-                <ul className = 'products'>
-                    <li className = 'product'>
-                        <ul className = 'order'>
-                            <li className = 'aviaticket'>Avia Ticket:</li>
-                            <li className = 'aviaticket_info'>
-                                <div className = 'passenger'>Zhanybekov Nartay</div>
-                                <div className = 'flight_info'>
-                                    <div className = 'cities'>Almaty - Astana</div>
-                                    <div className = 'date'>15:40, 13 august</div>
-                                </div>
-                                </li>
-                        </ul>
-                    </li>
-                </ul>
+            <div className="profile-row">
+                <div className="profile-key">Last Name:</div>
+                <div className="profile-value">Nursultan</div>
+            </div>
+            <div className="profile-row">
+                <div className="profile-key">Email:</div>
+                <div className="profile-value">Nursultan</div>
+            </div>
+            <div className="profile-row">
+                <div className="profile-key">Phone:</div>
+                <div className="profile-value">Nursultan</div>
             </div>
         </div>
-            )
-}
+        )
+};
+
 export default Profile;
