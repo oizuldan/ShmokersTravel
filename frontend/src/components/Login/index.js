5b0bed3fb25c0c8c3b71ba5d5b42b262c858f293
Nartay <nartay.zhanybekov@nu.edu.kz>
2019-11-01 09:35:03 +0600

    progile added

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import Modal from "react-bootstrap/Modal";
-
+import Profile from '../../routes/profile'
 import Button from "../Button";
 import {validateEmail} from "../../helpers/validate";
 
