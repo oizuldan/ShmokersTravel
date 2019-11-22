eb142dc6f62a24b790e7af5884ef4eacfb1b0b81
aisultankassenov <aisultan.kassenov@nu.edu.kz>
2019-11-22 16:38:41 +0600

    feat(backend): change sample sql

--- shmokerstravel/shmokerstravel/src/main/resources/migrations/insert_data.sql
+++ shmokerstravel/shmokerstravel/src/main/resources/migrations/insert_data.sql
@@ -21,17 +21,17 @@ VALUES('Pavlodar', 'Nur-Sultan');
 
 
 INSERT INTO trip(departure_train_station_name, arrival_train_station_name, departure_date, arrival_date)
-VALUES('Nur-Sultan', 'Petropavl', '2019-11-01T08:00', '2019-11-01T16:00'); -- 1
+VALUES('Nur-Sultan', 'Petropavl', '2019-12-01T08:00', '2019-12-01T16:00'); -- 1
 INSERT INTO trip(departure_train_station_name, arrival_train_station_name, departure_date, arrival_date)
-VALUES('Petropavl', 'Nur-Sultan', '2019-11-01T17:00', '2019-11-02T01:00'); -- 2
+VALUES('Petropavl', 'Nur-Sultan', '2019-12-01T17:00', '2019-12-02T01:00'); -- 2
 INSERT INTO trip(departure_train_station_name, arrival_train_station_name, departure_date, arrival_date)
-VALUES('Kokshetau', 'Kostanay', '2019-11-01T12:00', '2019-11-01T15:00'); -- 3
+VALUES('Kokshetau', 'Kostanay', '2019-12-01T12:00', '2019-12-01T15:00'); -- 3
 INSERT INTO trip(departure_train_station_name, arrival_train_station_name, departure_date, arrival_date)
-VALUES('Kostanay', 'Kokshetau', '2019-11-01T16:00', '2019-11-01T19:00'); -- 4
+VALUES('Kostanay', 'Kokshetau', '2019-12-01T16:00', '2019-12-01T19:00'); -- 4
 INSERT INTO trip(departure_train_station_name, arrival_train_station_name, departure_date, arrival_date)
-VALUES('Nur-Sultan', 'Pavlodar', '2019-11-01T12:00', '2019-11-01T15:00'); -- 5
+VALUES('Nur-Sultan', 'Pavlodar', '2019-12-01T12:00', '2019-12-01T15:00'); -- 5
 INSERT INTO trip(departure_train_station_name, arrival_train_station_name, departure_date, arrival_date)
-VALUES('Pavlodar', 'Nur-Sultan', '2019-11-01T16:00', '2019-11-01T19:00'); -- 6
+VALUES('Pavlodar', 'Nur-Sultan', '2019-12-01T16:00', '2019-12-01T19:00'); -- 6
 INSERT INTO trip(departure_train_station_name, arrival_train_station_name, departure_date, arrival_date)
 VALUES('Kokshetau', 'Kostanay', '2019-09-01T12:00', '2019-09-01T15:00'); -- 7
 
@@ -80,38 +80,38 @@ INSERT INTO seat(seat_number, vagon_number, train_number) VALUES(5,1,6);
 
 
 INSERT INTO trip_station(trip_id, station_index, train_station_name, arrival_date, departure_date)
-VALUES(1, 1, 'Nur-Sultan', NULL, '2019-11-01T08:00');
+VALUES(1, 1, 'Nur-Sultan', NULL, '2019-12-01T08:00');
 INSERT INTO trip_station(trip_id, station_index, train_station_name, arrival_date, departure_date)
-VALUES(1, 2, 'Kokshetau', '2019-11-01T11:00', '2019-11-01T11:00');
+VALUES(1, 2, 'Kokshetau', '2019-12-01T11:00', '2019-12-01T11:00');
 INSERT INTO trip_station(trip_id, station_index, train_station_name, arrival_date, departure_date)
-VALUES(1, 3, 'Petropavl', '2019-11-01T16:00', null);
+VALUES(1, 3, 'Petropavl', '2019-12-01T16:00', null);
 
 INSERT INTO trip_station(trip_id, station_index, train_station_name, arrival_date, departure_date)
-VALUES(2, 1, 'Petropavl', NULL, '2019-11-01T17:00');
+VALUES(2, 1, 'Petropavl', NULL, '2019-12-01T17:00');
 INSERT INTO trip_station(trip_id, station_index, train_station_name, arrival_date, departure_date)
-VALUES(2, 2, 'Kokshetau', '2019-11-01T22:00', '2019-11-01T22:00');
+VALUES(2, 2, 'Kokshetau', '2019-12-01T22:00', '2019-12-01T22:00');
 INSERT INTO trip_station(trip_id, station_index, train_station_name, arrival_date, departure_date)
-VALUES(2, 3, 'Nur-Sultan', '2019-11-02T01:00', null);
+VALUES(2, 3, 'Nur-Sultan', '2019-12-02T01:00', null);
 
 INSERT INTO trip_station(trip_id, station_index, train_station_name, arrival_date, departure_date)
-VALUES(3, 1, 'Kokshetau', NULL, '2019-11-01T12:00');
+VALUES(3, 1, 'Kokshetau', NULL, '2019-12-01T12:00');
 INSERT INTO trip_station(trip_id, station_index, train_station_name, arrival_date, departure_date)
-VALUES(3, 2, 'Kostanay', '2019-11-01T15:00', null);
+VALUES(3, 2, 'Kostanay', '2019-12-01T15:00', null);
 
 INSERT INTO trip_station(trip_id, station_index, train_station_name, arrival_date, departure_date)
-VALUES(4, 1, 'Kostanay', NULL, '2019-11-01T16:00');
+VALUES(4, 1, 'Kostanay', NULL, '2019-12-01T16:00');
 INSERT INTO trip_station(trip_id, station_index, train_station_name, arrival_date, departure_date)
-VALUES(4, 2, 'Kokshetau', '2019-11-01T19:00', null);
+VALUES(4, 2, 'Kokshetau', '2019-12-01T19:00', null);
 
 INSERT INTO trip_station(trip_id, station_index, train_station_name, arrival_date, departure_date)
-VALUES(5, 1, 'Nur-Sultan', NULL, '2019-11-01T12:00');
+VALUES(5, 1, 'Nur-Sultan', NULL, '2019-12-01T12:00');
 INSERT INTO trip_station(trip_id, station_index, train_station_name, arrival_date, departure_date)
-VALUES(5, 2, 'Pavlodar', '2019-11-01T15:00', null);
+VALUES(5, 2, 'Pavlodar', '2019-12-01T15:00', null);
 
 INSERT INTO trip_station(trip_id, station_index, train_station_name, arrival_date, departure_date)
-VALUES(6, 1, 'Pavlodar', NULL, '2019-11-01T16:00');
+VALUES(6, 1, 'Pavlodar', NULL, '2019-12-01T16:00');
 INSERT INTO trip_station(trip_id, station_index, train_station_name, arrival_date, departure_date)
-VALUES(6, 2, 'Nur-Sultan', '2019-11-01T19:00', null);
+VALUES(6, 2, 'Nur-Sultan', '2019-12-01T19:00', null);
 
 INSERT INTO trip_station(trip_id, station_index, train_station_name, arrival_date, departure_date)
 VALUES(7, 1, 'Kokshetau', NULL, '2019-09-01T12:00');
@@ -119,72 +119,72 @@ INSERT INTO trip_station(trip_id, station_index, train_station_name, arrival_dat
 VALUES(7, 2, 'Kostanay', '2019-09-01T15:00', null);
 
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(1, 1, 1, 1, 3, 1, 500, '2019-11-01T08:00');
+VALUES(1, 1, 1, 1, 3, 1, 500, '2019-12-01T08:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(2, 1, 1, 1, 3, 1, 500, '2019-11-01T08:00');
+VALUES(2, 1, 1, 1, 3, 1, 500, '2019-12-01T08:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(3, 1, 1, 1, 3, 1, 500, '2019-11-01T08:00');
+VALUES(3, 1, 1, 1, 3, 1, 500, '2019-12-01T08:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(4, 1, 1, 1, 3, 1, 500, '2019-11-01T08:00');
+VALUES(4, 1, 1, 1, 3, 1, 500, '2019-12-01T08:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(5, 1, 1, 1, 3, 1, 500, '2019-11-01T08:00');
+VALUES(5, 1, 1, 1, 3, 1, 500, '2019-12-01T08:00');
 
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(1, 1, 2, 1, 3, 2, 500, '2019-11-01T17:00');
+VALUES(1, 1, 2, 1, 3, 2, 500, '2019-12-01T17:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(2, 1, 2, 1, 3, 2, 500, '2019-11-01T17:00');
+VALUES(2, 1, 2, 1, 3, 2, 500, '2019-12-01T17:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(3, 1, 2, 1, 3, 2, 500, '2019-11-01T17:00');
+VALUES(3, 1, 2, 1, 3, 2, 500, '2019-12-01T17:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(4, 1, 2, 1, 3, 2, 500, '2019-11-01T17:00');
+VALUES(4, 1, 2, 1, 3, 2, 500, '2019-12-01T17:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(5, 1, 2, 1, 3, 2, 500, '2019-11-01T17:00');
-
-
-INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(1, 1, 3, 1, 2, 3, 500, '2019-11-01T12:00');
-INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(2, 1, 3, 1, 2, 3, 500, '2019-11-01T12:00');
-INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(3, 1, 3, 1, 2, 3, 500, '2019-11-01T12:00');
-INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(4, 1, 3, 1, 2, 3, 500, '2019-11-01T12:00');
-INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(5, 1, 3, 1, 2, 3, 500, '2019-11-01T12:00');
+VALUES(5, 1, 2, 1, 3, 2, 500, '2019-12-01T17:00');
 
 
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(1, 1, 4, 1, 2, 4, 500, '2019-11-01T16:00');
+VALUES(1, 1, 3, 1, 2, 3, 500, '2019-12-01T12:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(2, 1, 4, 1, 2, 4, 500, '2019-11-01T16:00');
+VALUES(2, 1, 3, 1, 2, 3, 500, '2019-12-01T12:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(3, 1, 4, 1, 2, 4, 500, '2019-11-01T16:00');
+VALUES(3, 1, 3, 1, 2, 3, 500, '2019-12-01T12:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(4, 1, 4, 1, 2, 4, 500, '2019-11-01T16:00');
+VALUES(4, 1, 3, 1, 2, 3, 500, '2019-12-01T12:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(5, 1, 4, 1, 2, 4, 500, '2019-11-01T16:00');
+VALUES(5, 1, 3, 1, 2, 3, 500, '2019-12-01T12:00');
+
 
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(1, 1, 5, 1, 2, 5, 500, '2019-11-01T12:00');
+VALUES(1, 1, 4, 1, 2, 4, 500, '2019-12-01T16:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(2, 1, 5, 1, 2, 5, 500, '2019-11-01T12:00');
+VALUES(2, 1, 4, 1, 2, 4, 500, '2019-12-01T16:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(3, 1, 5, 1, 2, 5, 500, '2019-11-01T12:00');
+VALUES(3, 1, 4, 1, 2, 4, 500, '2019-12-01T16:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(4, 1, 5, 1, 2, 5, 500, '2019-11-01T12:00');
+VALUES(4, 1, 4, 1, 2, 4, 500, '2019-12-01T16:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(5, 1, 5, 1, 2, 5, 500, '2019-11-01T12:00');
+VALUES(5, 1, 4, 1, 2, 4, 500, '2019-12-01T16:00');
 
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(1, 1, 6, 1, 2, 6, 500, '2019-11-01T16:00');
+VALUES(1, 1, 5, 1, 2, 5, 500, '2019-12-01T12:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(2, 1, 6, 1, 2, 6, 500, '2019-11-01T16:00');
+VALUES(2, 1, 5, 1, 2, 5, 500, '2019-12-01T12:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(3, 1, 6, 1, 2, 6, 500, '2019-11-01T16:00');
+VALUES(3, 1, 5, 1, 2, 5, 500, '2019-12-01T12:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(4, 1, 6, 1, 2, 6, 500, '2019-11-01T16:00');
+VALUES(4, 1, 5, 1, 2, 5, 500, '2019-12-01T12:00');
 INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
-VALUES(5, 1, 6, 1, 2, 6, 500, '2019-11-01T16:00');
+VALUES(5, 1, 5, 1, 2, 5, 500, '2019-12-01T12:00');
+
+INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
+VALUES(1, 1, 6, 1, 2, 6, 500, '2019-12-01T16:00');
+INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
+VALUES(2, 1, 6, 1, 2, 6, 500, '2019-12-01T16:00');
+INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
+VALUES(3, 1, 6, 1, 2, 6, 500, '2019-12-01T16:00');
+INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
+VALUES(4, 1, 6, 1, 2, 6, 500, '2019-12-01T16:00');
+INSERT INTO available_seat(seat_number, vagon_number, trip_id, from_station, to_station, train_number, ticket_price, date) 
+VALUES(5, 1, 6, 1, 2, 6, 500, '2019-12-01T16:00');
 
 INSERT INTO user(password, phone, email, first_name, last_name)
 VALUES("123", "87082079818", "aisultan.kassenov@nu.edu.kz", "Aisultan", "Kassenov");
